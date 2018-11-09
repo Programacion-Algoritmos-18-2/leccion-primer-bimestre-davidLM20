e6d9962039e0876efb3e549b49ea8309eb179b0e
David <jhandry13@hotmail.com>
2018-11-09 12:47:12 -0500

    Agregando ultima clase con comentarios

--- ejercicio1/mipaquete/modelado.py
+++ ejercicio1/mipaquete/modelado.py
@@ -66,34 +66,69 @@ class EmpleadoPorHoras(Empleado):
 	def presentar_datos(self):
 		cadena = "%s\n\tNumero horas: %s\n\tValor hora: %s\n\tSueldo Final: %s" % (super(EmpleadoPorHoras,self).presentar_datos(), self.obtener_numero_horas(), self.obtener_valor_horas(), self.calcular_valor_sueldo())
 		return cadena
-#Metodo expecial para los empleados fijos
+#Metodo especial para los empleados fijos
 class EmpleadoFijo(Empleado):
 	"""docstring for EmpleadoFijo"""
+	#se llama los atributos de la clase padre en este caso Empleado
 	def __init__(self):
 		super(EmpleadoFijo, self).__init__()
+		#se declara los atributos de la case empleado fijo 
 		self.sueldo_fijo = 0 
 		self.descuento_seguro = 0 
 		self.sueldo_final = 0.0
-
+	# estos dos metodos se usan para agregar el sueldo fijo y el descuento
 	def agregar_sueldo_fijo(self,s):
 		self.sueldo_fijo = s 
 
 	def agregar_descuento(self,d):
 		self.descuento_seguro = d
 
-
+	#estos dos metodos se usan para el retorno de los valores del sueldo fijo y de descuento
 	def obtener_sueldo_fijo(self):
 		return self.sueldo_fijo
 
 	def obtener_descuento(self):
 		return self.descuento_seguro
 
+	#este metodo se usa para calcular el sueldo final y lo retorna el el mismo metodo
 	def calcular_sueldo_final(self):
 		self.sueldo_final = ((self.sueldo_fijo - ((self.sueldo_fijo * self.descuento_seguro)/100)) + super(EmpleadoFijo,self).obtener_comision_fija())
 		return self.sueldo_final
 
+	#este metodo se usa para presentar y generear una nueva cadena con los valores que corresponde a empleado fijo
 	def presentar_datos(self):
 		cadena = "%s\n\tSueldo Fijo: %s\n\tDescuento del seguro:%s\n\tSueldo Final:%s" %(super(EmpleadoFijo,self).presentar_datos(), self.obtener_sueldo_fijo(), self.obtener_descuento(), self.calcular_sueldo_final())
 		return cadena
 
-class 				
+##Metodo especial para los empleados por semana
+class EmpleadoPorSemana(Empleado):
+	"""docstring for EmpleadoFijo"""
+	#se llama a los atributos de la clase padre en este caso de Empleado
+	def __init__(self):
+		super(EmpleadoPorSemana, self).__init__()
+		#Se crea las variables privadas para esta clse
+		self.numero_semana = 0 
+		self.valor_semanal = 0 
+		self.sueldo_final = 0.0
+	#Estos dos metodos son para agregar el numero de semanas y el valor de cada una
+	def agregar_numero_semana(self,ns):
+		self.numero_semana = ns 
+
+	def agregar_valor_semana(self,vs):
+		self.valor_semanal = vs
+	#Estos dos metodos son para retornar el valor de nuemro de semanas y el valor de cada una
+	def obtener_numero_semana(self):
+		return self.numero_semana
+
+	def obtener_valor_semana(self):
+		return self.valor_semanal
+
+	#este metodo se usa para calcular el sueldo final del empleado esta ves en semanas
+	def calcular_sueldo_final(self):
+		self.sueldo_final = (self.numero_semana * self.valor_semanal)
+		return self.sueldo_final
+
+	#este metodo presenta los valores de la calse empleado por semana y los datos de la calse padre empleado	
+	def presentar_datos(self):
+		cadena = "%s\n\tNumero de semanas:%s\n\tValor Semanal:%s\n\tSueldo Final:%s" %(super(EmpleadoPorSemana,self).presentar_datos(), self.obtener_numero_semana(), self.obtener_valor_semana(), self.calcular_sueldo_final())
+		return cadena				
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -1,13 +1,15 @@
-
+#Se importa todo lo que tinene el paquete
 from mipaquete.modelado import *
+#se crea el objeto para agregar valores y presentar usando la clase padre
 e = Empleado()
 e.agregar_nombre("Luis")
 e.agregar_apellido("Benitez")
 e.agregar_cedula("1110001")
 print(e.presentar_datos())
 
+#se crea una segunda clase para agregar valores y presentar datos esta ves se usara la clase empleado por hora y los valores de la clase padre
 e1 = EmpleadoPorHoras()
-nombre = input("Ingrese el nombre:\n")
+nombre = input("Ingrese el nombre:\n")# se pide por teclado el nombre
 e1.agregar_nombre(nombre)
 e1.agregar_apellido("Andrade")
 e1.cedula = "112233"
@@ -15,11 +17,20 @@ e1.agregar_valor_horas(20.2)
 e1.agregar_numero_horas(15)
 print(e1.presentar_datos()) 
 
+#se crea un nuevo objeto este trabajara con la clase empleado fijo
 e2 = EmpleadoFijo()
 e2.agregar_sueldo_fijo(300.2)
 e2.descuento_seguro = 10
-comision = float(input("Ingrese comision:\n"))
+comision = float(input("Ingrese comision:\n")) # se pide la comision y se lo declara como objeto
 e2.agregar_comision_fija(comision)
 e2.nombre = "Ana"
 e2.apellido = "Diaz"
 print(e2.presentar_datos()) 
+
+#este objeto es para comprobar la funcionalidad de la clase empelado por semana
+e3 = EmpleadoPorSemana()
+e3.agregar_numero_semana(3)
+e3.agregar_valor_semana(80)
+e3.nombre = "David"
+e3.apellido = "Diaz"
+print(e3.presentar_datos()) 
